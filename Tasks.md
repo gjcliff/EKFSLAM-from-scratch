5b496a6e145008c3b36b5328cfc48a8fa3d60877
Graham Clifford <gjcliff@gmail.com>
2024-01-06 14:59:21 -0600

    Completed task A.2, I think!
    I added all .stl, .rviz, and .urdf.xacro files to the repository and
    added an install function to the CMakeLists file as well. I also added
    an install function for the launch directory. I added a find_package(rclcpp Required)
    line as well, since the ROS documentation did that and I assume I'll need
    that too eventually.
    
    I'm not sure if my directory hierarchy is what matt wants, but I'm pretty
    sure it'll work and I like it so that's how it's going to stay for now.
    
    Modified urdf files to reference files within my package, and not
    turtlebot3_description. I'll need to update the file extensions of those files
    to .urdf.xacro instead of .urdf, but I will do that in nthe next commit
    since I forgot this time.
    
    Put the launchfile in declarative style as well.

--- Tasks.md
+++ Tasks.md
@@ -1,2 +1,2 @@
 Task A.1: Complete 1/5/2024
-Task A.2: Complete 
+Task A.2: Complete 1/6/2024
--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -7,36 +7,32 @@ from launch.conditions import LaunchConfigurationEquals
 
 
 def generate_launch_description():
-    rviz_arg = DeclareLaunchArgument(
-        "use_rviz", default_value="true",
-        description="true (default): determines whether or not rviz is launched"
-    ),
-    jsp_arg = DeclareLaunchArgument(
-        "use_jsp", default_value="true",
-        description="true (default): determines whether ot not the joint_state_publisher\
-        is used to publish joint states"
-    ),
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        on_exit=Shutdown(),  # I think this will work because this is a kwarg of ExecuteLocal
-        condition=LaunchConfigurationEquals("usue_rviz", "true"),
-        arguement=["-d",
-                   PathJoinSubstitution(
-                       [FindPackageShare(
-                           "nuturtle_description"), "config", "basic_purple.rviz"]
-                   )]
-    ),
-    jsp_node = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        condition=LaunchConfigurationEquals("use_jsp", "true")
-    ),
     return LaunchDescription([
-        rviz_arg,
-        jsp_arg,
-        rviz_node,
-        jsp_node,
+        DeclareLaunchArgument(
+            "use_rviz", default_value="true",
+            description="true (default): determines whether or not rviz is launched"
+        ),
+        DeclareLaunchArgument(
+            "use_jsp", default_value="true",
+            description="true (default): determines whether ot not the joint_state_publisher\
+            is used to publish joint states"
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            on_exit=Shutdown(),  # I think this will work because this is a kwarg of ExecuteLocal
+            condition=LaunchConfigurationEquals("usue_rviz", "true"),
+            arguement=["-d",
+                       PathJoinSubstitution(
+                           [FindPackageShare(
+                            "nuturtle_description"), "config", "basic_purple.rviz"]
+                       )]
+        ),
+        Node(
+            package="joint_state_publisher",
+            executable="joint_state_publisher",
+            condition=LaunchConfigurationEquals("use_jsp", "true")
+        ),
         RegisterEventHandler(
 
         )
