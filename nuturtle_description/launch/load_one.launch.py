2ff123cbf3570dd6f2afdf4c4dba8482bd785a3b
Graham Clifford <gjcliff@gmail.com>
2024-01-07 21:29:40 -0600

    The launch file works and step A.4 is complete
    AFter much troubleshooting, the launchfile works and everything is
    loaded in correctly with the correct prefixes and everyitng. now I must
    go around and add separate rviz files for each color and add logic to
    the launch file for using them.

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -1,48 +1,66 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import PythonExpression
 from launch.actions import DeclareLaunchArgument, Shutdown
-from launch.substitutions import PathJoinSubstitution, Command
+from launch.substitutions import PathJoinSubstitution, Command, LaunchConfiguration, EqualsSubstitution
 from launch_ros.substitutions import FindPackageShare, ExecutableInPackage
-from launch.conditions import LaunchConfigurationEquals
+from launch.conditions import IfCondition
 
 
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             "use_rviz", default_value="true",
-            description="true (default): determines whether or not rviz is launched"
+            description="determines whether or not rviz is launched"
         ),
         DeclareLaunchArgument(
             "use_jsp", default_value="true",
-            description="true (default): determines whether ot not the joint_state_publisher\
-            is used to publish joint states"
+            description="determines whether ot not the joint_state_publisher is used to publish joint states"
         ),
-        Node(
-            package="rviz2",
-            executable="rviz2",
-            on_exit=Shutdown(),  # I think this will work because this is a kwarg of ExecuteLocal
-            condition=LaunchConfigurationEquals("usue_rviz", "true"),
-            arguement=["-d",
-                       PathJoinSubstitution(
-                           [FindPackageShare(
-                            "nuturtle_description"), "config", "basic_purple.rviz"]
-                       )]
-        ),
-        Node(
-            package="joint_state_publisher",
-            executable="joint_state_publisher",
-            condition=LaunchConfigurationEquals("use_jsp", "true")
+        DeclareLaunchArgument(
+            "color", default_value="purple",
+            description="determines the color of the turtlebot in rviz",
+            choices=["red", "green", "blue", "purple"]
         ),
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
+            namespace=LaunchConfiguration("color"),
             parameters=[
                 {"robot_description":
                  Command([ExecutableInPackage("xacro", "xacro"), " ",
                           PathJoinSubstitution(
                               [FindPackageShare(
-                                  "nuturtle_description"), "turtlebot3_burger.urdf.xacro"])])}
-            ]
-        )
+                                  "nuturtle_description"), "urdf", "turtlebot3_burger.urdf.xacro"])]),
+                 # I tried so many things, this was hard to figure out
+                 "frame_prefix": PythonExpression(["'", LaunchConfiguration('color'), "/'"])
+                 }
+            ],
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            namespace=LaunchConfiguration("color"),
+            on_exit=Shutdown(),  # I think this will work because this is a kwarg of ExecuteLocal
+            condition=IfCondition(EqualsSubstitution(
+                LaunchConfiguration("use_rviz"), "true")),
+            arguments=["-d",
+                       PathJoinSubstitution(
+                           [FindPackageShare(
+                            "nuturtle_description"), "rviz", "basic_purple.rviz"]
+                       ),
+                       "-f", PythonExpression(
+                           ["'", LaunchConfiguration('color'), "/base_link'"])
+                       ]
+
+        ),
+        Node(
+            package="joint_state_publisher",
+            executable="joint_state_publisher",
+            namespace=LaunchConfiguration("color"),
+            condition=IfCondition(EqualsSubstitution(
+                LaunchConfiguration("use_jsp"), "true"))
+        ),
+
 
     ])
